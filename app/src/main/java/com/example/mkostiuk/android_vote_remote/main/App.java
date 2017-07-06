b5819ee99b948a727aa67c52c50137de402115cc
MKostiuk <Mat01061995>
2017-07-06 12:09:02 +0200

    Ajout popup demande accès stockage au démarrage

--- app/src/main/java/com/example/mkostiuk/android_vote_remote/main/App.java
+++ app/src/main/java/com/example/mkostiuk/android_vote_remote/main/App.java
@@ -1,11 +1,15 @@
 package com.example.mkostiuk.android_vote_remote.main;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,6 +38,8 @@ import xdroid.toaster.Toaster;
 import static org.fourthline.cling.binding.xml.Descriptor.Device.ELEMENT.service;
 
 public class App extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
 
     private Button one, two, three, four, five, six, seven, eight, nine, zero, inscription, retour, affiheQuestion;
     private Service service;
@@ -129,7 +135,34 @@ public class App extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
-        init();
+
+        //Vérification que l'autorisation d'accès au système de stockage est accrodée
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                //Cela signifie que la permission à déjà était
+                //demandé et l'utilisateur l'a refusé
+                //Vous pouvez aussi expliquer à l'utilisateur pourquoi
+                //cette permission est nécessaire et la redemander
+                Toaster.toast("Vous avez refusé l'accés au Stockage, fermeture");
+                finish();
+            } else {
+                //Sinon demander la permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_STORAGE);
+            }
+        }
+        else {
+            //Permission déjà accrodée
+            init();
+        }
+
+
+
     }
 
     public void onClickOne(View view) throws TransformerException, ParserConfigurationException {
@@ -223,5 +256,24 @@ public class App extends AppCompatActivity {
     public void sendVide() {
         service.getVoteRemoteLocalService().getManager().getImplementation()
                 .envoieCommande("");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // La permission est garantie on initialise les services et boutons
+                    init();
+                } else {
+                    Toaster.toast("Permission refusée, fermeture");
+                    finish();
+                }
+                return;
+            }
+        }
     }
 }
